4e73a456db6cadb7c2ab7459e6c3f4c633878d7b
Adriano Bono <adriano@radek.com.br>
2023-10-01 20:23:48 -0300

    feat(dev-003): added generic button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,21 @@
+import React, { ButtonHTMLAttributes } from "react";
+import styles from "./Button.module.scss";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: any;
+}
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <button
+        type="button"
+        className={styles["products-button"]}
+        {...props}
+        ref={ref}
+      >
+        {children}
+      </button>
+    );
+  }
+);
