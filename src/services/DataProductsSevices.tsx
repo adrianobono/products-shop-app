fff760905de3bf7e4500c4018bd879f5d65de740
Adriano Bono <adriano@radek.com.br>
2023-10-01 15:00:12 -0300

    feat(dev-002): initial validate api to get products

---
+++ src/services/DataProductsSevices.tsx
@@ -0,0 +1,16 @@
+import { AxiosError } from "axios";
+import { productsApi } from "../api";
+
+export const getProducts = async (): Promise<any> => {
+  try {
+    const { data } = await productsApi.get("/");
+
+    return data;
+  } catch (error) {
+    const err = error as AxiosError;
+    console.log(err.message);
+    console.log(err.name);
+
+    return [];
+  }
+};
