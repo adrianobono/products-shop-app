edb9ed2041f81042f3f192b90e2c3be193f55e0b
Adriano Bono <adriano@radek.com.br>
2023-10-01 20:27:19 -0300

    feat(dev-003): initial set  for reset styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./routes/routes";
+import { QueryClient } from "@tanstack/query-core";
+import { QueryClientProvider } from "@tanstack/react-query";
+import "./index.scss";
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     {/* added router for pages navigation */}
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
   </React.StrictMode>
 );
