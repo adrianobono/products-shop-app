fc31a4fbe821d2fe4993220bae95799984ee4729
Adriano Bono <adriano@radek.com.br>
2023-10-01 21:18:13 -0300

    feat(dev-003): set cache time to load products

--- src/components/ProductstList/ProductsList.tsx
+++ src/components/ProductstList/ProductsList.tsx
@@ -7,12 +7,11 @@ import Loading from "../Loading/Loading";
 
 export const ProductsList = () => {
   const { data: productsList, isLoading } = useQuery(
-    ["getAllProducts", "products"],
-    () => getProducts()
+    ["products"],
+    () => getProducts(),
+    { cacheTime: 5000 }
   );
-  if (isLoading || !productsList) {
-    return <Loading />;
-  }
+  if (isLoading || !productsList) return <Loading />;
 
   return (
     <ul className={styles["products-list__wrapper"]}>
