37d083a52c5d1e1355ee1b2c2bdc707979e0bcfa
Adriano Bono <adriano@radek.com.br>
2023-10-03 14:41:10 -0300

    feat(dev-005): updated layout and functionality

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -17,36 +17,30 @@ export const ProductCard = ({ product }: ProductsListProps) => {
 
   const handleSetQuantity = (value: number) => {
     let sum = quantity + value;
+    product.quantity = sum;
 
-    value === -1 && removeFromCart(product.id);
+    value === -1 && removeFromCart(product);
     value === 1 && addToCart(product);
 
     sum >= 0 && setQuantity(sum);
+    console.log(cart);
   };
 
-  const handleCrud = (type: string, id: number) => {
-    type === "del" && deleteProduct(id);
-  };
+  const itemCart = cart.filter((item: any) => item.id === product.id);
 
   return (
     <li className={styles["product-card__item"]} key={product.id}>
-      <span className={styles["product-card__buttons-crud"]}>
-        <Button
-          onClick={() => handleCrud("del", product.id)}
-          className={styles["product-card__button-delete"]}
-        >
-          Delete
-        </Button>
-        <Button className={styles["product-card__button-edit"]}>Edit</Button>
-      </span>
       <img src={product.srcImage} alt={product.altImage} />
       <p>{product.title}</p>
       <span>${product.value}</span>
       <span className={styles["product-card__buttons-shop"]}>
-        <Button disabled={quantity === 0} onClick={() => handleSetQuantity(-1)}>
+        <Button
+          disabled={itemCart[0]?.quantity === 0 || !itemCart[0]}
+          onClick={() => handleSetQuantity(-1)}
+        >
           Remove
         </Button>
-        <span> {quantity} </span>
+        <span>{itemCart[0]?.quantity | 0}</span>
         <Button onClick={() => handleSetQuantity(1)}>Add to cart</Button>
       </span>
     </li>
