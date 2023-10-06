b63db707ae75208eb7a53b39a948ec6dcc4477d7
Adriano Bono <adriano@radek.com.br>
2023-10-06 02:28:25 -0300

    feat(dev-006): updated layout

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,38 +1,40 @@
-import { Button } from "../Button";
-import { useEffect, useState } from "react";
-import { ProductsDTO } from "../../types/dto";
-
-import styles from "./ProductCard.module.scss";
+import { useEffect } from "react";
 import { useCartStore } from "../../store/CartStore";
+import { Button } from "../Button";
+import { ProductsDTO } from "../../types/dto";
+import styles from "./ProductCard.module.scss";
 
 interface ProductsListProps {
-  products: ProductsDTO[];
   product: ProductsDTO;
+  products: ProductsDTO[];
 }
 
-export const ProductCard = ({ products, product }: ProductsListProps) => {
+export const ProductCard = ({ product, products }: ProductsListProps) => {
   const { cart, addToCart, removeFromCart, setProducts } = useCartStore();
+
+  const handleSetQuantity = (cartQty: number, value: number) => {
+    product.quantity = !cartQty ? 1 : +cartQty + value;
+
+    // logic by value to add or remove a product at cart
+    value === -1 && removeFromCart(product);
+    value === 1 && addToCart(product);
+  };
+
+  //filter to get the same product cart to compare parameters
+  const itemCart = cart.filter((item: any) => item.id === product.id);
 
   useEffect(() => {
     setProducts(products);
   }, []);
 
-  const handleSetQuantity = (cartQty: number, value: number) => {
-    product.quantity = !cartQty ? 1 : +cartQty + value;
-
-    value === -1 && removeFromCart(product);
-    value === 1 && addToCart(product);
-  };
-
-  const itemCart = cart.filter((item: any) => item.id === product.id);
-
   return (
-    <li className={styles["product-card__item"]} key={product.id}>
+    <li className={styles["list__item"]} key={product.id}>
       <img src={product.srcImage} alt={product.altImage} />
       <p>{product.title}</p>
       <span>${product.value}</span>
-      <span className={styles["product-card__buttons-shop"]}>
+      <span className={styles["list__buttons-shop"]}>
         <Button
+          data-testid="button-cart-remove"
           disabled={itemCart[0]?.quantity === 0 || !itemCart[0]}
           onClick={() => handleSetQuantity(itemCart[0]?.quantity, -1)}
         >
@@ -40,6 +42,7 @@ export const ProductCard = ({ products, product }: ProductsListProps) => {
         </Button>
         <span>{itemCart[0]?.quantity | 0}</span>
         <Button
+          data-testid="button-cart-add"
           disabled={product.quantity >= product.stock}
           onClick={() => handleSetQuantity(itemCart[0]?.quantity, 1)}
         >
